3b718119d01e0468f1eb883b4b73d0ec127278da
md-alif007 <mohammadalif043@gmail.com>
2025-02-18 14:24:02 +0600

    election vote count

---
+++ election.js
@@ -0,0 +1,27 @@
+function electionResult(votes) {
+
+    if (Array.isArray(votes) === false) {
+        return "Invalid";
+    }
+
+    let mango = 0;
+    let banana = 0;
+    for (const element of votes) {
+        if (element === "mango") {
+            mango++;
+        }
+        if (element === "banana") {
+            banana++;
+        }
+    }
+
+    if (mango > banana) {
+        return "Mango";
+    } else if (banana > mango) {
+        return "Banana";
+    } else if (mango === banana) {
+        return "Draw";
+    }
+}
+console.log(electionResult(["mango", "BananA", "na vote", "na vote"]));
+
